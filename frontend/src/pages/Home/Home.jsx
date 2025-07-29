031acc1b172e4faa0d8c4c6de40bb537175197ef
Elom <believeramedji@gmail.com>
2025-07-29 15:03:44 +0200

    Réorganisation du kasa; ajout du frontend

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from 'react'
 import Card from '../../components/Card/Card'
 import Banner from '../../components/Banner/Banner'
@@ -15,21 +14,20 @@ const Home = () => {
   }, [])
 
   return (
-  <div className="home">
-  <Banner image="/assets/banner-home.jpg" title="Chez vous, partout et ailleurs" />
+    <div className="home">
+      <Banner image="/assets/banner-home.jpg" title="Chez vous, partout et ailleurs" />
 
-  <div className="home__grid">
-    {properties.map((property) => (
-      <Card
-        key={property.id}
-        id={property.id}
-        title={property.title}
-        cover={property.cover}
-      />
-    ))}
-  </div>
-</div>
-
+      <div className="home__grid">
+        {properties.map((property) => (
+          <Card
+            key={property.id}
+            id={property.id}
+            title={property.title}
+            cover={property.cover}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/router/Router.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import Logement from "../pages/Logement";
+import NotFound from "../pages/NotFound/NotFound";
+import Layout from "../layout/Layout";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="logement/:id" element={<Logement />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRouter;
